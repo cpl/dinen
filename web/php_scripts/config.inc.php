0afecc9e0f02b745dd217f820a46b052e6cc5810
Alexandru-Paul Copil <copil.alexander@gmail.com>
2017-02-11 18:15:15 +0000

    Purge

--- web/php_scripts/config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db_host = 'dinen.ddns.net';
-$db_user = 'teamdinen';
-$db_pass = 'dinenx3';
-$db_name = 'dinen';
--- web/php_scripts/connect_to_db.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once 'config.inc.php';
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
