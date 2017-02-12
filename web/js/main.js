53419fb2617a2324c28113f5643933ce95824780
stefan <stefan.pristoleanu@student.manchester.ac.uk>
2017-02-12 18:33:28 +0200

    Added alert pannels and JSON communication with PHP server. (main.js)

--- web/js/main.js
+++ web/js/main.js
@@ -1,31 +1,8 @@
-// Here we will write the code responsible for communicating between frontend and backend
-//this function modifies the registrationForm submit in order to send JSON data to server:
-$(function () {
-    $('#registerForm').on('submit', function (e) {
-        e.preventDefault();
-        //var dataParam = $(this).serialize();
-        //console.log("my data param: " + dataParam);
-        var dataJson = formToJSON("#registerForm");
-        console.log("my data JSON: " + JSON.stringify(dataJson));
-    });
-});
+// Here we will write the code responsible for communicating between frontend and backend 
 
-function login() {
-  $.ajax(
-    {
-      url: 'php_scripts/login.php',
-      type: 'POST',
-      data : { email : $("#email").val(), password : $("#password").val() }
-   }).done(goToRestaurants);
-   return false;
-}
+var serverURL = ""; //  or: "/api/" put the server url here
 
-function goToRestaurants()
-{
-  window.location.replace("restaurants.php");
-}
-
-//this function compare password with c_password:
+//this function compare password with c_password: 
 function validatePassword() {
     var password = document.getElementById("password");
     var confirm_password = document.getElementById("c_password");
@@ -34,6 +11,34 @@ function validatePassword() {
     } else {
         confirm_password.setCustomValidity('');
     }
+}
+
+//this function modifies the registrationForm submit in order to send JSON data to server:
+$(function () {
+    $('#registerForm').on('submit', function (e) {
+        e.preventDefault();
+        var dataJson = formToJSON("#registerForm");
+        console.log("my data JSON: " + JSON.stringify(dataJson));
+        requestWsPost(serverURL + "register.php", dataJson, "processRegisterResponse");
+    });
+});
+
+function processRegisterResponse(response) {
+    console.log("processRegisterResponse: " + JSON.stringify(response));
+    // sessionStorage.setItem("lastResponseMessage",  response.message); // save message in browser session for other pages ...
+    if (response.result === 'OK') {
+        showMsgAlert("msg-info.html", response.message);
+    } else {
+        showMsgAlert("msg-error.html", response.message);
+    }
+}
+
+function showMsgAlert(component, message) {
+    $.ajax({method: "GET", url: "components/" + component, success: function (data) {
+            data = data.toString().replace("#msg#", message);
+            $("#msgDiv").replaceWith(data);
+            $("#msgDiv").fadeIn(800);
+        }});
 }
 
 function formToJSON(selectedForm) {
@@ -48,4 +53,36 @@ function formToJSON(selectedForm) {
         }
     });
     return json;
+}
+
+/**
+ * _requestWsPost - private method for RESTFul API POST call
+ * @param {string} urlCmd = serverURL + wsRoot + wsCmd
+ * @param {string} requestData
+ * @param {string} callBackFN = numele functiei care va fi apelata cand primim raspunsul din server
+ */
+function requestWsPost(urlCmd, requestData, callBackFN) {
+    $.post(urlCmd, requestData)
+            .done(function (data) {
+                //console.log("_requestWsPost resp: " + JSON.stringify(data) + " - " + callBackFN);
+                _processAllWSResponse(data, callBackFN);
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                if (jqXHR.status && jqXHR.status === 400) {
+                    console.log('Communication error: ' + jqXHR.responseText);
+                } else {
+                    console.log('Something went wrong: communication error!');
+                }
+            });
+}
+
+/**
+ * _processAllWSResponse - private method only for requestWsPost 
+ * @param {string} responseData
+ * @param {string} callBackFN
+ * @returns {undefined}
+ */
+function _processAllWSResponse(responseData, callBackFN) {
+    //console.log('_processAllWSResponse - callBackFN: ' + callBackFN + " - responseData: " + JSON.stringify(responseData));
+    this[callBackFN](responseData);
 }
