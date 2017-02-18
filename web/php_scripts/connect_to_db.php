8bd6e0b3157c4be49a0c082529ff2fb169c07cb7
Nikita <fritee@mail.ru>
2017-02-18 18:48:54 +0000

    Fixed an error

--- web/php_scripts/connect_to_db.php
+++ web/php_scripts/connect_to_db.php
@@ -3,6 +3,7 @@
 function createMysqlConnection()
 {
   require_once 'config.inc.php';
+  global $db_host, $db_user, $db_pass, $db_name;
   $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
   return $mysqli;
 }
