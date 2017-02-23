8e3e8feba19aec4c3f3e9c62e383cb2a4f79df1d
Ben Cripps <ben.cripps@student.manchester.ac.uk>
2017-02-23 06:16:30 +0000

    Implemented JWT creator

--- web/api/v1/api.php
+++ web/api/v1/api.php
@@ -1,9 +1,11 @@
 <?php
 header('Content-Type: application/json');
+require_once '../../php_scripts/config.inc.php';
 require_once '../../php_scripts/register.php';
 require_once '../../php_scripts/login.php';
 require_once '../../php_scripts/create_restaurant.php';
 $request = htmlspecialchars($_POST['request']);
+const DATE_TIME_FORMAT = DateTime::ATOM;
 switch ($request) {
   case 'register':
     processRegisterRequest();
@@ -52,4 +54,31 @@ function processLoginRequest() {
     }
   }
   echo json_encode(login($email, $password));
+}
+
+/* Create a JSON Web Token for post-login user authentication (expires after
+   six hours). I assume users can be uniquely identified by email. Refer to
+   https://tools.ietf.org/html/rfc7519 for information on JWTs. */
+function generateJWT($user_email, $user_name, $user_category) {
+  $header = base64_encode(json_encode(['alg' => 'HS256', 'typ' => 'JWT']));
+  $nowInUnixTime = time();
+  $sixHoursInSeconds = 6 * 60 * 60;
+  # Generate a random id; create a series of random bytes, then encode them in
+  # base 64 (so the id is a number).
+  $tokenID = base64_encode(random_bytes(32));
+  $payload = base64_encode(json_encode([
+    'iss' => 'https://dinen.ddns.net/api/v1',
+    'sub' => $user_email,
+    'aud' => 'https://dinen.ddns.net',
+    'exp' => $nowInUnixTime + $sixHoursInSeconds,
+    'nbf' => $nowInUnixTime,
+    'iat' => $nowInUnixTime,
+    'jti' => $tokenID,
+    'user_name' => $user_name,
+    'user_category' => $user_category
+  ]));
+  global $api_secret;
+  $signature = base64_encode(hash_hmac('sha256', $header.'.'.$payload,
+                                       $api_secret));
+  return $header.'.'.$payload.'.'.$signature;
 }
--- web/php_scripts/config.inc.php
+++ web/php_scripts/config.inc.php
@@ -3,3 +3,4 @@ $db_host = 'dinen.ddns.net';
 $db_user = 'teamdinen';
 $db_pass = 'dinenx3';
 $db_name = 'dinen';
+$api_secret = "oh:shit;it's:the:'lum1nat1.txt";
