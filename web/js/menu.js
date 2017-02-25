bb9e7c68710034310c6ca337acabae814924f055
Nikita <fritee@mail.ru>
2017-02-25 15:48:52 +0000

    Added menu items processing, remove restrict_access.php, some other small code changes

---
+++ web/js/menu.js
@@ -0,0 +1,57 @@
+var apiURL = 'api/v1/api.php';
+
+var Status = { ERROR: 0, SUCCESS: 1 };
+
+$(function () {
+  get_menu();
+});
+
+// Get menu for restaurant
+// Needs to have 'menu' (menu id) and 'restaurant' (restaurant id)
+// as parameters
+function get_menu()
+{
+  // if menu GET parameter doesn't exist, abort operation
+  if(!('menu' in get_url_vars()) || !('restaurant' in get_url_vars())) {
+    console.log("No menu / restaurant parameter in GET");
+    return;
+  }
+  var requestData = {'request': 'get_menu',
+                     'menu_id' : get_url_vars()['menu'],
+                     'restaurant_id' : get_url_vars()['restaurant']};
+  $.ajax({
+    url: apiURL,
+    type: 'POST',
+    data: requestData
+  }).done(generate_html_for_menu);
+
+  return false;
+}
+
+function generate_html_for_menu(response)
+{
+  if (response.status === Status.SUCCESS) {
+    $('#menu').append('Menu items: <br>');
+    response.data.forEach(function (item) {
+      $('#menu').append('Menu item: ' + item.name + ' in a ' +
+                                item.section + '. Cost: $' + item.cost +
+                                '. Description: ' + item.description + '.<br>');
+    });
+  } else {
+    console.log(response);
+  }
+}
+
+// function to get GET parameters
+// Used as getUrlVars()['parameter']
+// Copied from stack overflow answer
+// Kinda surprised that neither JS nor JQuery has a url parameter getters
+function get_url_vars()
+{
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+    function(m,key,value) {
+      vars[key] = value;
+    });
+    return vars;
+}
