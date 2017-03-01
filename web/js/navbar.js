967aec8541f285001f26e320e58d11aad77b26c6
Adam Cowdy <cowdyadam@gmail.com>
2017-03-01 13:26:31 +0000

    Various navbar improvements

--- web/js/navbar.js
+++ web/js/navbar.js
@@ -1,15 +1,32 @@
-if($('.navbar').length > 0){
+if($('.navbar').length > 0 && $('.navbar').hasClass('navbar-fixed-top')) {
   $(window).on('scroll load resize', function () {
     checkScroll();
   });
 }
 
+$('.navbar-toggle').click(function() {
+  if ($('.navbar-toggle').hasClass('collapsed')) {
+    arguments.callee.opaqueNavbar = $('.navbar-toggle').hasClass('opaque');
+    turnNavbarOpaque();
+  } else if (!arguments.callee.opaqueNavbar) {
+    turnNavbarTransparent();
+  }
+});
+
 function checkScroll() {
   // The point where the navbar changes in px.
   var startY = $('.navbar').height() * 2;
   if ($(window).scrollTop() > startY) {
-    $('.navbar').addClass("scrolled");
+    turnNavbarOpaque();
   } else {
-    $('.navbar').removeClass("scrolled");
+    turnNavbarTransparent();
   }
+}
+
+function turnNavbarOpaque() {
+  $('.navbar').addClass("opaque");
+}
+
+function turnNavbarTransparent() {
+  $('.navbar').removeClass("opaque");
 }
