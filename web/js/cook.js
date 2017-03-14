4fd086f76d48fe7646053f1ab94af3894b619358
Nikita <fritee@mail.ru>
2017-03-14 09:00:18 +0000

    Added ordering capabilities

--- web/js/cook.js
+++ web/js/cook.js
@@ -1,12 +1,14 @@
 var apiURL = 'api/v1/api.php';
 
 var Status = {ERROR: 0, SUCCESS: 1};
-
+var menuLoaded = false;
+var menuItems = {};
 
 $(function(){
-  getOrders();
   getMenu();
 });
+
+window.setInterval(getOrders, 10000);
 
 // Get unifinished orders for restaurant
 // Needs to have 'restaurant' (restaurant id)
@@ -24,7 +26,7 @@ function getOrders()
     url: apiURL,
     type: 'POST',
     data: requestData
-  });//.done(processItems);
+  }).done(processOrderItems);
 }
 
 // Get menu for restaurant
@@ -43,9 +45,38 @@ function getMenu()
     url: apiURL,
     type: 'POST',
     data: requestData
-  });
+  }).done(processMenuItems);
 
   return false;
+}
+
+function processOrderItems(response)
+{
+  // TODO: Check for errors
+  // don't process items if menu isn't loaded
+  if(!menuLoaded)
+    return;
+  $('#order-items').empty();
+  var data = response['data'];
+  data.forEach(function(orderItem){
+    var string = "<tr>" +
+                 "<th>" + orderItem.order_id + "</th>" +
+                 "<th>" + menuItems[orderItem.menu_item_id].name + "</th>" +
+                 "<th>" + "TODO" + "</th>" +
+                 "</tr>";
+    $('#order-items').append(string);
+  });
+}
+
+function processMenuItems(response)
+{
+  menuLoaded = true;
+  var data = response['data'];
+  data.forEach(function(menuItem){
+    menuItems[menuItem.id] = menuItem;
+  });
+  console.log(menuItems);
+  getOrders();
 }
 
 function get_url_vars()
