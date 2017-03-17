0432fcd5fe18d9299270cb0cf9484c49b3b009f7
Ben Cripps <ben.cripps@student.manchester.ac.uk>
2017-03-17 14:33:24 +0000

    Begun SPA

--- web/js/common.js
+++ web/js/common.js
@@ -26,31 +26,6 @@ $(function () {
   });
 });
 
-function register(event) {
-  var ref = $(this).find("[required]");
-  $(ref).each(function () {
-    if ($(this).val() === '') {
-      alert("Required field should not be blank.");
-      $(this).focus();
-      event.preventDefault();
-      return false;
-    }
-  });
-  $.ajax({
-    url: apiURL,
-    type: 'POST',
-    data: 'request=register&data=' + formToJSON('#registerForm')
-  }).done(function (response) {
-    if (response == 'success') {
-      alert('Registration email was sent to your email');
-      window.location.replace('index.html');
-    } else {
-      alert(response);
-    }
-  });
-  return false;
-}
-
 function login() {
   $.ajax({
     url: apiURL,
