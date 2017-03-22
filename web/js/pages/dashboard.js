e6a48d8f5a877c119f100225b28e27ce7e4e1930
stefan <stefan.pristoleanu@student.manchester.ac.uk>
2017-03-23 01:58:47 +0200

    Added a delete restaurant JS function and modal panel

--- web/js/pages/dashboard.js
+++ web/js/pages/dashboard.js
@@ -1,69 +1,108 @@
 function Dashboard() {
-  var me = this;
-  this.init = function () {
-    me.allRestaurants = "";
-    me.oneRestaurant = $('#section').html();
-    $('#section').html("");
-    me.getRestaurants();
+    var me = this;
+    this.init = function () {
+        me.allRestaurants = "";
+        me.oneRestaurant = $('#section').html();
+        $('#section').html("");
+        me.getRestaurants();
 
-    $('#to-index').click(function(event){
-      loadPage('landing');
-      event.preventDefault();
-    });
+        $('#to-index').click(function (event) {
+            loadPage('landing');
+            event.preventDefault();
+        });
 
-    $('#create-restaurant').click(function(event){
-      loadPage('register_restaurant');
-      event.preventDefault();
-    });
-  };
+        $('#create-restaurant').click(function (event) {
+            loadPage('register_restaurant');
+            event.preventDefault();
+        });
 
-  this.getRestaurants = function () {
+        $('#delete_restaurant').click(function (event) {
+            console.log("merge");
+            event.preventDefault();
+            me.deleteRestaurant();
+        });
+    };
+
+    this.getRestaurants = function () {
+        const JWT = getJWT();
+        if (JWT == null) {
+            loadPage('landing');
+            return false;
+        }
+
+        var requestData = {'request': 'get_restaurants', 'jwt': JWT};
+        $.ajax({
+            url: apiURL,
+            type: 'POST',
+            data: requestData
+        }).done(me.listRestaurants);
+        
+        return false;
+    };
+
+    this.listRestaurants = function (response) {
+        if (response.status === Status.SUCCESS) {
+            console.log(JSON.stringify(response));
+            response.data.forEach(me.listRestaurant);
+            $("#section").replaceWith(me.allRestaurants);
+        } else {
+            $('#welcome').innerHTML = response.data;
+        }
+    };
+
+    this.listRestaurant = function (restaurant) {
+        var tempRestaurant = me.oneRestaurant;
+        tempRestaurant = tempRestaurant.toString().replace("#name#",
+                restaurant.name);
+        tempRestaurant = replaceAll(tempRestaurant.toString(), "#id#", restaurant.id);
+        tempRestaurant = tempRestaurant.toString().replace("#description#",
+                restaurant.description);
+        tempRestaurant = tempRestaurant.toString().replace('#menu#',
+                'return editRestaurantMenu(' + restaurant.id + ')');
+        tempRestaurant = tempRestaurant.toString().replace('#cookModule#',
+                'return gotoCookModule(' + restaurant.id + ')');
+        me.allRestaurants += tempRestaurant;
+    };
+    
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
+}
+
+function deleteRestaurant(id) {
+    console.log("deleted restaurant: " + id);
     const JWT = getJWT();
-    if (JWT == null) {
-      loadPage('landing');
-      return false;
-    }
+        if (JWT == null) {
+            loadPage('landing');
+            return false;
+        }
+    var requestData = {'request': 'remove_restaurant','restaurant_id': id, 'jwt': JWT};
+        $.ajax({
+            url: apiURL,
+            type: 'POST',
+            data: requestData
+        }).done(processResponseDeleteRestaurant);
+}
 
-    var requestData = {'request': 'get_restaurants', 'jwt': JWT};
-    $.ajax({
-      url: apiURL,
-      type: 'POST',
-      data: requestData
-    }).done(me.listRestaurants);
-    return false;
-  };
-
-  this.listRestaurants = function (response) {
-    if (response.status === Status.SUCCESS) {
-      response.data.forEach(me.listRestaurant);
-      $("#section").replaceWith(me.allRestaurants);
-    } else {
-      $('#welcome').innerHTML = response.data;
-    }
-  };
-
-  this.listRestaurant = function (restaurant) {
-    var tempRestaurant = me.oneRestaurant;
-    tempRestaurant = tempRestaurant.toString().replace("#name#",
-      restaurant.name);
-    tempRestaurant = tempRestaurant.toString().replace("#description#",
-      restaurant.description);
-    tempRestaurant = tempRestaurant.toString().replace('#menu#',
-      'return editRestaurantMenu(' + restaurant.id + ')');
-    tempRestaurant = tempRestaurant.toString().replace('#cookModule#',
-      'return gotoCookModule(' + restaurant.id + ')');
-    me.allRestaurants += tempRestaurant;
-  };
+function processResponseDeleteRestaurant(response) {
+    console.log(JSON.stringify(response));
+        if (response.status === Status.SUCCESS) {
+            alert("Successfully deleted the restaurant.");
+        }
+        else{
+            alert("Error deleting the restaurant.");
+        }
 }
 
 function editRestaurantMenu(id) {
-  sessionStorage.setItem('restaurantID', id);
-  loadPage('menu');
-  return false;
+    sessionStorage.setItem('restaurantID', id);
+    loadPage('menu');
+    return false;
 }
 
 function gotoCookModule(id) {
-  sessionStorage.setItem('restaurantID', id);
-  loadPage('cook');
-  return false;
+    sessionStorage.setItem('restaurantID', id);
+    loadPage('cook');
+    return false;
 }
--- web/js/pages/sign_in.js
+++ web/js/pages/sign_in.js
@@ -5,6 +5,7 @@ function signIn() {
     data: 'request=login&data=' + formToJSON('#sign_in_form')
   }).done(function (response) {
     if (response.status === Status.SUCCESS) {
+       console.log(JSON.stringify(response));
       localStorage.setItem('JWT', response.data);
       loadPage('dashboard');
       initHeader();
