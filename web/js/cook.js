52f43941b9c455844393b99cc87dc3a1f45247d6
Stefan's Tablet <Stefan's Tablet@10.186.234.203>
2017-03-22 13:05:19 +0000

    Added a comment section in the other pages

--- web/js/cook.js
+++ web/js/cook.js
@@ -65,6 +65,7 @@ function Cook()
               "<th>" + me.menuItems[orderItem.menu_item_id].name + "</th>" +
               "<th>" + /* Add means of identifying customer */ +"</th>" +
               "<th>" + orderItem.time + "</th>" +
+              "<th>" + orderItem.comments + "</th>" +
               "<th>" + me.genOrderItemCheckbox(orderItem) +
               "</tr>";
           $('#order-items').append(string);
